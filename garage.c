b7e87511adba870e9780d99f93323b1a991c7e31
yothinin <yothinin@hotmail.com>
2020-06-09 22:46:46 +0700

    Move function to under main function.

--- garage.c
+++ garage.c
@@ -24,8 +24,119 @@ enum{
   N_COLUMNS
 };
 
+GtkTreeModel *init_model(void);
+void icon_clicked(GtkWidget *widget, GtkTreePath *tree_path, gpointer user_data);
+static gboolean exitprog(void);
+static gboolean delete_event(GtkWidget *window, GdkEvent *event, gpointer parent);
+
+int main(int argc, char *argv[]){
+  GtkWidget *icon_view;
+  GtkWidget *sw;
+  
+  gtk_init(&argc, &argv);
+  
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  
+  gtk_window_set_title(GTK_WINDOW(window), "ระบบพิมพ์ใบเสร็จ");
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+
+  gtk_window_set_icon_from_file(GTK_WINDOW(window), "./resources/bulb.png", 0);
+  
+  sw = gtk_scrolled_window_new(NULL, NULL);
+  gtk_container_add(GTK_CONTAINER(window), sw);
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw), 
+    GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+  gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw), 
+    GTK_SHADOW_IN);
+    
+  icon_view = gtk_icon_view_new_with_model(init_model());
+  gtk_container_add(GTK_CONTAINER(sw), icon_view);
+  
+  gtk_icon_view_set_text_column(GTK_ICON_VIEW(icon_view), 
+    COL_DISPLAY_NAME);
+  gtk_icon_view_set_pixbuf_column(GTK_ICON_VIEW(icon_view), COL_PIXBUF);
+  gtk_icon_view_set_selection_mode(GTK_ICON_VIEW(icon_view), 
+    GTK_SELECTION_SINGLE);
+    
+  g_signal_connect(G_OBJECT(icon_view), 
+      "selection_changed", G_CALLBACK(icon_clicked), list_store);
+  
+  g_signal_connect(GTK_OBJECT(window), 
+      "delete_event", G_CALLBACK(delete_event),(gpointer) window);
+  
+  gtk_widget_show_all(window);
+  gtk_main();
+  
+  return 0;
+}
+
+GtkTreeModel *init_model(void){
+  GdkPixbuf *p1, *p2, *p3, *p4, *p5;
+  GtkTreeIter iter;
+  GError *err = NULL;  
+  
+  p1 = gdk_pixbuf_new_from_file("./resources/customer.png", &err);
+  p2 = gdk_pixbuf_new_from_file("./resources/parts.png", &err);
+  p3 = gdk_pixbuf_new_from_file("./resources/invoice.png", &err);
+  p4 = gdk_pixbuf_new_from_file("./resources/settings.png", &err);
+  p5 = gdk_pixbuf_new_from_file("./resources/exit.png", &err);
+  
+  assert(err == NULL);
+  
+  list_store = gtk_list_store_new(NUM_COLS, G_TYPE_STRING, GDK_TYPE_PIXBUF);
+  
+  gtk_list_store_append(list_store, &iter);
+  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "ลูกค้า", COL_PIXBUF, p1, -1);
+  gtk_list_store_append(list_store, &iter);
+  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "สต็อค", COL_PIXBUF, p2, -1);
+  gtk_list_store_append(list_store, &iter);
+  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "ออกบิล", COL_PIXBUF, p3, -1);
+  gtk_list_store_append(list_store, &iter);
+  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "ตั้งค่า", COL_PIXBUF, p4, -1);
+  gtk_list_store_append(list_store, &iter);
+  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "เลิกใช้งาน", COL_PIXBUF, p5, -1);
+  
+  g_object_unref(p1);
+  g_object_unref(p2);
+  g_object_unref(p3);
+  g_object_unref(p4);
+  g_object_unref(p5);
+  
+  return GTK_TREE_MODEL(list_store);
+}
+
+void icon_clicked(GtkWidget *widget, GtkTreePath *tree_path, gpointer user_data){
+  GtkTreeModel *model = gtk_icon_view_get_model(GTK_ICON_VIEW(widget));
+  GtkTreeIter iter;
+ 
+  GList *sel = gtk_icon_view_get_selected_items(GTK_ICON_VIEW(widget));
+   
+  if (sel != NULL){
+    GtkTreePath *path;
+    path = sel->data;
+
+    gtk_tree_model_get_iter(model, &iter, path);
+    if (gtk_tree_model_get_iter(model, &iter, path)){
+      gchar *str;
+      gtk_tree_model_get(model, &iter, 0, &str, -1);
+      gint *idx = gtk_tree_path_get_indices(path);
+      g_print("%d -> %s\n", idx[0], str);
+      
+      switch (idx[0]){
+        case 4:
+          exitprog();
+          break;
+        default:
+          break;
+      }
+    }
+  }      
+}
+
 static gboolean
-exitprog(){
+exitprog(void){
   
   gtk_widget_hide(window);
   
@@ -80,114 +191,4 @@ delete_event(GtkWidget *window, GdkEvent *event, gpointer parent){
   }else{
     return TRUE;
   }
-}
-
-void icon_clicked(GtkWidget *widget, GtkTreePath *tree_path, gpointer user_data){
-  GtkTreeModel *model = gtk_icon_view_get_model(GTK_ICON_VIEW(widget));
-  GtkTreeIter iter;
- 
-  GList *sel = gtk_icon_view_get_selected_items(GTK_ICON_VIEW(widget));
-   
-  if (sel != NULL){
-    GtkTreePath *path;
-    path = sel->data;
-
-	gtk_tree_model_get_iter(model, &iter, path);
-    if (gtk_tree_model_get_iter(model, &iter, path)){
-      gchar *str;
-      gtk_tree_model_get(model, &iter, 0, &str, -1);
-      gint *idx = gtk_tree_path_get_indices(path);
-      g_print("%d -> %s\n", idx[0], str);
-      
-      switch (idx[0]){
-        case 4:
-          exitprog();
-          break;
-        default:
-          break;
-      }
-    }
-  }      
-}
-
-GtkTreeModel *init_model(void){
-  GdkPixbuf *p1, *p2, *p3, *p4, *p5;
-  GtkTreeIter iter;
-  GError *err = NULL;
-  
-  p1 = gdk_pixbuf_new_from_file("./resources/customer.png", &err);
-  p2 = gdk_pixbuf_new_from_file("./resources/parts.png", &err);
-  p3 = gdk_pixbuf_new_from_file("./resources/invoice.png", &err);
-  p4 = gdk_pixbuf_new_from_file("./resources/settings.png", &err);
-  p5 = gdk_pixbuf_new_from_file("./resources/exit.png", &err);
-  
-  assert(err == NULL);
-  
-  list_store = gtk_list_store_new(NUM_COLS, G_TYPE_STRING, GDK_TYPE_PIXBUF);
-  
-  gtk_list_store_append(list_store, &iter);
-  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "ลูกค้า", COL_PIXBUF, p1, -1);
-  gtk_list_store_append(list_store, &iter);
-  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "สต็อค", COL_PIXBUF, p2, -1);
-  gtk_list_store_append(list_store, &iter);
-  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "ออกบิล", COL_PIXBUF, p3, -1);
-  gtk_list_store_append(list_store, &iter);
-  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "ตั้งค่า", COL_PIXBUF, p4, -1);
-  gtk_list_store_append(list_store, &iter);
-  gtk_list_store_set(list_store, &iter, COL_DISPLAY_NAME, "เลิกใช้งาน", COL_PIXBUF, p5, -1);
-  
-  g_object_unref(p1);
-  g_object_unref(p2);
-  g_object_unref(p3);
-  g_object_unref(p4);
-  g_object_unref(p5);
-  
-  return GTK_TREE_MODEL(list_store);
-}
-
-int main(int argc, char *argv[]){
-  GtkWidget *icon_view;
-  GtkWidget *sw;
-  //GdkPixbuf *ico;
-  
-  gtk_init(&argc, &argv);
-  
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  
-  gtk_window_set_title(GTK_WINDOW(window), "ระบบพิมพ์ใบเสร็จ");
-  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-  gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
-
-  //ico = gdk_pixbuf_new_from_file("./resources/bulb.png", 0);
-  //gtk_window_set_icon(GTK_WINDOW(window), ico);
-  
-  gtk_window_set_icon_from_file(GTK_WINDOW(window), "./resources/bulb.png", 0);
-  
-  sw = gtk_scrolled_window_new(NULL, NULL);
-  gtk_container_add(GTK_CONTAINER(window), sw);
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw), 
-    GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-  gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw), 
-    GTK_SHADOW_IN);
-    
-  icon_view = gtk_icon_view_new_with_model(init_model());
-  gtk_container_add(GTK_CONTAINER(sw), icon_view);
-  
-  gtk_icon_view_set_text_column(GTK_ICON_VIEW(icon_view), 
-    COL_DISPLAY_NAME);
-  gtk_icon_view_set_pixbuf_column(GTK_ICON_VIEW(icon_view), COL_PIXBUF);
-  gtk_icon_view_set_selection_mode(GTK_ICON_VIEW(icon_view), 
-    GTK_SELECTION_SINGLE);
-    
-  g_signal_connect(G_OBJECT(icon_view), "selection_changed", G_CALLBACK(icon_clicked), list_store);
-  //g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-  
-  g_signal_connect(GTK_OBJECT(window), 
-         "delete_event", G_CALLBACK(delete_event),(gpointer) window);
-  
-  gtk_widget_show_all(window);
-  gtk_main();
-  
-  return 0;
 }
