c9c269e3759897d57279e33581b37402559d2628
yothinin <yothin@hotmail.com>
2020-06-11 17:33:09 +0700

    Change window to dialog.

--- gtktreeview.c
+++ gtktreeview.c
@@ -5,22 +5,38 @@ enum{
   COL_AGE, 
   NUM_COLS
 };
+
 static GtkTreeModel *create_and_fill_model (void);
 static GtkWidget *create_view_and_model(void);
 
 int main(int argc, char *argv[]){
-  GtkWidget *win;
+  //GtkWidget *win;
   GtkWidget *view;
+  
+  GtkWidget *button , *hbox, *dlg;
   
   gtk_init(&argc, &argv);
   
-  win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  g_signal_connect(win, "delete_event", gtk_main_quit, NULL);
+  dlg = gtk_dialog_new();
+  gtk_window_set_title(GTK_WINDOW(dlg), "ลูกค้า");
+  gtk_window_set_position(GTK_WINDOW(dlg), GTK_WIN_POS_CENTER);
+  gtk_widget_set_size_request(dlg, 400, 300);
+  
+  hbox = gtk_hbox_new(FALSE, 0);
+  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dlg)->vbox), hbox);
+  
+  //win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  //g_signal_connect(win, "delete_event", gtk_main_quit, NULL);
   
   view = create_view_and_model();
-  gtk_container_add(GTK_CONTAINER(win), view);
+  //gtk_container_add(GTK_CONTAINER(win), view);
+  gtk_box_pack_start(GTK_BOX(hbox), view, FALSE, FALSE, 0);
   
-  gtk_widget_show_all(win);
+  button = gtk_button_new_from_stock(GTK_STOCK_QUIT);
+  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dlg)->action_area), button);
+  
+  gtk_widget_show_all(dlg);
   gtk_main();
   return 0;
 }
@@ -36,7 +52,7 @@ static GtkWidget *create_view_and_model(void){
   gtk_tree_view_insert_column_with_attributes(
     GTK_TREE_VIEW(view), 
     -1, 
-    "Name", 
+    "รหัส", 
     renderer, 
     "text", COL_NAME, NULL);
   
@@ -44,7 +60,7 @@ static GtkWidget *create_view_and_model(void){
   gtk_tree_view_insert_column_with_attributes(
     GTK_TREE_VIEW(view), 
     -1, 
-    "Age", 
+    "ชื่อ-นามสกุล", 
     renderer, 
     "text", COL_AGE, NULL);
   
